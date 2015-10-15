2a21afa4e9af772d40e8dbe55e4f81b967131694
Raphael Brunner <raphi.brunner@me.com>
2015-10-15 14:46:21 +0200

    added task 5 currying

---
+++ uebung5.hs
@@ -0,0 +1,51 @@
+-- Aufgabe 1
+--a)
+{-
+Präzendenz : Operator
+10         : ((f a) b) links;
+9          : (.) rechts;
+8          : (^) rechts;
+7          : (*) links; (/) links;
+6          : (+) links; (-) links;
+5          : (:) rechts; (++) rechts; 
+4          : (==); (/=); (<=); (>=); (>); (<); 
+3          : 
+2          : (||) rechts; (&&) rechts;
+1          : 
+-}
+--b)
+-- ((((1 + (2 ^ 3)) == 6) && ((3 / 4) < 12)) || (snd(1, True)))
+-- (((3:) []) == (map (*5)[(2 ^ (4 ^ 6))]))
+
+-- Aufgabe 2
+--a)
+curry1 :: ((a,b) -> c) -> a -> b -> c
+curry1 f a b = f(a,b)
+
+--b)
+uncurry1 :: (a -> b -> c) -> (a,b) -> c
+uncurry1 a (b,c) = a b c
+
+-- Aufgabe 3
+--a)b)
+flip1 :: (b -> a -> c) -> a -> b -> c
+flip1 f a b = f b a
+
+flipl :: (b -> a -> c) -> a -> b -> c
+flipl = \f a b -> f b a
+
+-- Aufgabe 4
+--a)
+{-
+f.f
+g.f
+f.h
+i.g
+g.h
+-}
+
+--b)
+{-
+f.fst.i
+f.snd.i
+-}
