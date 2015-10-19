acfbc9e39589619399af5970741dc0c8f310262e
Raphael Brunner <raphi.brunner@me.com>
2015-10-20 00:53:31 +0200

    started implementation of fancy Diagramm

--- FirstDiagram.hs
+++ FirstDiagram.hs
@@ -5,7 +5,7 @@ import Diagrams.Backend.SVG.CmdLine
 
 -- Don't forget to add a type signature (:: Diagram B) to your creation
 main = withArgs (words "-o diagram.svg -w 400") (defaultMain diag)
-  where diag = (kunst 10) :: Diagram B
+  where diag = (fancyShit 20) :: Diagram B
 
 atopExample, besideExample, aboveExample :: Diagram B
 atopExample   = fc black (square 1) `atop` circle 1
@@ -36,4 +36,21 @@ kunst' n d = case odd n of
                         True -> kunst' (n-1) (d + 1) `atop` (rotateBy (d / 6) (fc white (square (fromIntegral n))))
                         False -> kunst' (n-1) (d + 1) `atop` (rotateBy (d / 6) (fc black (square (fromIntegral n))))
 
--- d)
+-- d) (optional)
+fancyShit :: Int -> Diagram B
+fancyShit n = fancyShit' n
+
+fancyShit' :: Int -> Diagram B
+fancyShit' 3 = fc beige (regPoly 3 3  # lw none)
+fancyShit' n = case mod n 11 of
+    0 -> fancyShit' (n - 1) `atop` (fc aqua (regPoly (fromIntegral n) (fromIntegral n) # lw none))
+    1 -> fancyShit' (n - 1) `atop` (fc aquamarine (regPoly (fromIntegral n) (fromIntegral n) # lw none))
+    2 -> fancyShit' (n - 1) `atop` (fc azure (regPoly (fromIntegral n) (fromIntegral n) # lw none))
+    3 -> fancyShit' (n - 1) `atop` (fc beige (regPoly (fromIntegral n) (fromIntegral n) # lw none))
+    4 -> fancyShit' (n - 1) `atop` (fc bisque (regPoly (fromIntegral n) (fromIntegral n) # lw none))
+    5 -> fancyShit' (n - 1) `atop` (fc blue (regPoly (fromIntegral n) (fromIntegral n) # lw none))
+    6 -> fancyShit' (n - 1) `atop` (fc blueviolet (regPoly (fromIntegral n) (fromIntegral n) # lw none))
+    7 -> fancyShit' (n - 1) `atop` (fc cadetblue (regPoly (fromIntegral n) (fromIntegral n) # lw none))
+    8 -> fancyShit' (n - 1) `atop` (fc chartreuse (regPoly (fromIntegral n) (fromIntegral n) # lw none))
+    9 -> fancyShit' (n - 1) `atop` (fc coral (regPoly (fromIntegral n) (fromIntegral n) # lw none))
+    10 -> fancyShit' (n - 1) `atop` (fc darkcyan (regPoly (fromIntegral n) (fromIntegral n) # lw none))
